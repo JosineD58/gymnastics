e1a54c6659ba60cf22b7e17a8c544ceeb872a62e
JosineD58 <106514121+JosineD58@users.noreply.github.com>
2022-06-25 15:26:22 +0200

    Navbar scss

--- src/pages/NavBar.tsx
+++ src/pages/NavBar.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Routes, Route, NavLink } from "react-router-dom";
+import "../styles/NavBar.scss";
 
 const NavBar = () => {
   return (
-    <div>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/checklists">Checklists</NavLink>
+    <div className="navbar">
+      <NavLink className="navlink" to="/">
+        Home
+      </NavLink>
+      <NavLink className="navlink" to="/about">
+        About
+      </NavLink>
+      <NavLink className="navlink" to="/checklists">
+        Checklists
+      </NavLink>
     </div>
   );
 };
